4bdf4ac9b0c64653a21937565b85d89b81d37d98
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:27:13 +0100

    Add: Manager für Techtree bezogene Funktionen

---
+++ GameManager/TechTreeManager.gd
@@ -0,0 +1,19 @@
+class_name TechTreeManager
+extends Node
+
+
+func _init():
+	print("NetworkManager Created")
+	pass
+
+
+func _ready():
+	pass
+
+
+func _enter_tree():
+	pass
+
+
+func _exit_tree():
+	pass
