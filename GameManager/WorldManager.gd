59c1e110e92a1b1d774bcfbcce2c1e93a2dd32f7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:26:57 +0100

    Add: Manager für Spielwelt bezogene Funktionen

---
+++ GameManager/WorldManager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+#
+#	Verwaltung der einzelnen Karten Ansichten der Spieler
+
+# Jeder Spieler hat die gleiche Karte in der er seine Einheiten steuern kann
+# Schickt einer der Spieler einen Angriffs Trupp zum Gegner ist es ihm unter bestimmten voraussetzungen 
+# Möglich (TechTreeUpgrade) Auf die Karte des anderen Spielers zu schauen und einfluss auf seinen Angriff 
+# zu nehmen.
+
+# Dazu ist es notwendig das seine Kamera die Karte des anderen Spielers anzeigt.
+
+# Dieser Manager ist ebenfalls dafür zuständig das die Karten Identische bei Start generiert werden.
+# Zusammen mit dem NetworkManager wird diese bei allen Spielern Synchronisiert
+
+# Über einen Worldseed soll es möglich sein eine Karte jederzeit bei Spielstart wieder neu zu generieren.
+
+
+
+func _init():
+	print("WorldManager Created")
+	pass
+
+
+func _ready():
+	pass
