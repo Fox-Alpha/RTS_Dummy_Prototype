2fd27aed7cf10dbd8e4e3284342dbf6e34fd5121
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:29:20 +0100

    Add: Manager für Resourcen bezogene Funktionen

---
+++ GameManager/ResourceManager.gd
@@ -0,0 +1,19 @@
+class_name ResourceManager
+extends Node
+
+
+func _init():
+	print("ResourceManager Created")
+	pass
+
+
+func _ready():
+	pass
+
+
+func _enter_tree():
+	pass
+
+
+func _exit_tree():
+	pass
