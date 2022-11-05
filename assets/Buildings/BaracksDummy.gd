098d83be7a7bf63e4cb0a6d5c46890c2bcdfb412
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:22:10 +0100

    Add: Platzhalterobject für Kaserne (Gebäude)

---
+++ assets/Buildings/BaracksDummy.gd
@@ -0,0 +1,14 @@
+class_name BarracksDummy
+extends Barracks
+
+
+func _ready():
+	pass
+
+# RegisterBuilding
+# BuildNewUnit
+# SpawnUnitAt
+# SetRallyPoint
+# MoveUnitToRallyPoint
+# CheckResourceForNewUnit
+# GetResourceForNewUnit
