6ba36cd97fc6f6a52613878d0b571899c49e3849
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:23:23 +0100

    Add: Basisklasse für Managerklassen

---
+++ GameManager/ManagerBase.gd
@@ -0,0 +1,28 @@
+extends Node
+class_name ManagerBase
+
+# ===========================
+# Bas Klasse von der alle Managerklassen abgeleitet werden
+# 
+# Nur gemeinsame Eigenschaften und Funktionen
+# 
+# 
+# 
+# ===========================
+
+func _init():
+	print("ManagerBase Created")
+	pass
+
+
+func _ready():
+	print("ManagerBase _ready()")
+	pass
+
+
+func _enter_tree():
+	pass
+
+
+func _exit_tree():
+	pass
