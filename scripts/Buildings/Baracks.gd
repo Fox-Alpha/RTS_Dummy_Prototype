96601e0e21b37888a25b9107b3601af4be1907ea
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:15:52 +0100

    Add: Objektklasse für Kasernen

---
+++ scripts/Buildings/Baracks.gd
@@ -0,0 +1,11 @@
+class_name Barracks
+extends BuildingBase
+
+
+onready var UnitSpawn = get_node("%UnitSpawnPosition3D")
+
+
+func _ready():
+	_buildingtype = "BuildingArmyBarrack"
+	_buildingname = "Army Barrack"
+	pass
