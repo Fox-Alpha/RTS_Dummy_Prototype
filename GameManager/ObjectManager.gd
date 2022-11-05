bd9d3c11ea4e71cbfb0b30fc19d22b43fbb730bd
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:30:25 +0100

    Add: Manager für Interaktive Objekte. ===: Leitet Ereignisse zwischen GameManager und dem ensprechen ObjektManager weiter

---
+++ GameManager/ObjectManager.gd
@@ -0,0 +1,43 @@
+class_name ObjectManager
+extends Node
+
+# ===========================
+# Manager der als Proxy zwischen interagierbaren Objekten steht
+# Hier wird ermittelt welches Objekt eine AKtion ausführt
+# und leitet diese weiter
+# ===========================
+
+signal objectselected
+signal objectunselected
+
+# ===========================
+func _init():
+	print("ObjectManager Created")
+	connect("objectselected", self, "_on_select_object")
+	connect("objectunselected", self, "_on_unselect_object")
+	pass
+
+
+func _ready():
+	pass
+
+
+func _enter_tree():
+	pass
+
+
+func _exit_tree():
+	pass
+
+# ===========================
+# Signal ein Objekt deselektieren
+func _on_unselect_object() -> void:
+#	selected_object = null
+	pass
+
+# Signal ein Objekt selektieren
+func _on_select_object(_selectedobject) -> void:
+	
+	pass
+# ===========================
+# ===========================
