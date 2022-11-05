b0b68da82758ef72b1360a24b590ad4fd4a8acec
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-05 12:21:13 +0100

    Add: Netzwerk Spezifische Scripte für den Multiplayer ===: BLANKO als Platzhalter damit die Verzeichnisse ins GIT übernommen werden

---
+++ Networking/NetworkManager.gd
@@ -0,0 +1,25 @@
+class_name NetworkManager
+extends Node
+
+# ===========================
+# 
+# Manager zum Steuern der Netzwerkkummunikation zwischen anderen Managern und den Spielern
+# 
+# 
+# ===========================
+
+func _init():
+	print("NetworkManager Created")
+	pass
+
+
+func _ready():
+	pass
+
+
+func _enter_tree():
+	pass
+
+
+func _exit_tree():
+	pass
