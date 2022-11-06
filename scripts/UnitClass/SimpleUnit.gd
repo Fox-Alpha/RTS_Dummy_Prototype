a1a07103ead206bff6609c449ea1af79ad6ecfce
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-06 17:48:34 +0100

    Add: ObjectType damit einen Unit erkannt werden kann

--- scripts/UnitClass/SimpleUnit.gd
+++ scripts/UnitClass/SimpleUnit.gd
@@ -2,6 +2,10 @@ class_name SimpleUnit
 extends UnitBase
 
 
+export var _ObjectType:int = 2 
+# setget _set_objecttype, get_objecttype
+
+
 func _ready():
 	UnitName = "Simple Dummy Unit"
 	UnitType = "SimpleDummyUnit"
