2e5de8301925a090dbd3a681f3f57801223f1272
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-06 11:15:39 +0100

    Add: Funktion zum Abfragen des Objecttypes hinzugefügt

--- scripts/BaseClasses/BuildingBase.gd
+++ scripts/BaseClasses/BuildingBase.gd
@@ -1,13 +1,14 @@
 class_name BuildingBase
 extends Node
 
-export var _canselectbuilding : bool = true
 export var _buildingname : String = "EmptyName" setget set_building_name,get_building_name
 export var _buildingtype : String = "EmptyType" setget set_building_type,get_building_type
+export var _ObjectType:int = 1 setget _set_objecttype, get_objecttype
 
 
-onready var GM:GameManager = get_tree().get_root().get_node("Main/GameManager")
-onready var BuildingRootNode = owner
+onready var GM = .get_tree().get_root().get_node("Main/GameManager")
+#var GMInstance setget set_gamemanagerinstance, _get_gamemanagerinstance
+onready var BuildingRootNode = get_parent()
 
 
 func _ready():
@@ -22,8 +23,14 @@ func _exit_tree():
 	pass
 
 
-func can_selected() -> bool:
-	return _canselectbuilding
+func _set_objecttype(value):
+	_ObjectType = value
+	pass
+
+
+func get_objecttype():
+	return _ObjectType
+	pass
 
 
 func set_building_name(value:String) -> void:
