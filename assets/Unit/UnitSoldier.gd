9da0895a427b5b675f41633d783b71f4a1cd0428
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-11-09 09:45:44 +0100

    Fix: Signal des NavAgents wieder verbunden

--- assets/Unit/UnitSoldier.gd
+++ assets/Unit/UnitSoldier.gd
@@ -2,12 +2,17 @@ extends Node
 
 
 onready var _ObjectTypeNode = get_node("%ObjectType")
+onready var _NavAgentNode = get_node_or_null("%NavigationAgent")
 
 
 func _ready():
 	_ObjectTypeNode._ObjectType = Globals.OBJECT_TYPE_ENUM.TYPE_UNIT
 	_ObjectTypeNode.UnitType = "UnitSoldierBase"
 	_ObjectTypeNode.UnitName = "Basic Soldier"
+	if _NavAgentNode == null:
+		_ObjectTypeNode._canmoveunit = false
 	pass
 
 
+func _on_NavigationAgent_velocity_computed(safe_velocity: Vector3) -> void:
+	pass # Replace with function body.
