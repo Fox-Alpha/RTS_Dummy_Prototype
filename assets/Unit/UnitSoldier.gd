32e2664a6c314c2c6dad65633905297dce88997d
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-09 22:46:21 +0100

    Add: try to navigate the Agent ===: But dosnt work

--- assets/Unit/UnitSoldier.gd
+++ assets/Unit/UnitSoldier.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 var is_selected : bool = false
+var is_moving : bool = false
+var speed = 5.0
 
 onready var _ObjectTypeNode = get_node("%ObjectType")
 onready var _NavAgentNode = get_node_or_null("%NavigationAgent")
@@ -15,16 +17,29 @@ func _ready():
 	_ObjectTypeNode.UnitName = "Basic Soldier"
 	if _NavAgentNode == null:
 		_ObjectTypeNode._canmoveunit = false
-	pass
+
+
+func _physics_process(delta):
+	if is_moving:
+		var MoveToThisPosition = _NavAgentNode.get_next_location()
+		var velocity = (MoveToThisPosition - $Unit.transform.origin).normalized() * speed * delta
+	
+#		$Unit.move_and_collide(velocity)
+		_NavAgentNode.set_velocity(velocity)
 
 
 func _on_NavigationAgent_velocity_computed(_safe_velocity: Vector3) -> void:
-	pass # Replace with function body.
+		$Unit.move_and_collide(_safe_velocity)
+#	pass # Replace with function body.
 
 
 func SetAgentTarget(newAgentTarget:Vector3)-> void:
 	print_debug("SetAgentTarget: %s" % newAgentTarget)
 	_NavAgentNode.set_target_location(newAgentTarget)
+	var reachable = _NavAgentNode.is_target_reachable()
+	if reachable:
+		print_debug("is_target_reachable(): %s" % reachable)
+#	_NavAgentNode.
 
 
 func can_objectselected() -> bool:
@@ -42,3 +57,19 @@ func select_object(selected:bool) -> void:
 		shader.set_shader_param("strenght", 1.0)
 	else:
 		shader.set_shader_param("strenght", 0.0)
+
+
+func _on_NavigationAgent_path_changed():
+	is_moving = true
+#	pass # Replace with function body.
+
+
+func _on_NavigationAgent_target_reached():
+	is_moving = false
+#	pass # Replace with function body.
+
+
+func _on_NavigationAgent_navigation_finished():
+	print_debug("_on_NavigationAgent_navigation_finished()")
+
+#	pass # Replace with function body.
