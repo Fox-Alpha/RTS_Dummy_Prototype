8bd615684b2d6e01460fc0d944130a81192a6282
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-09 21:03:55 +0100

    Add: Weitere Eigenschaften ergänzt ===: getter und setter

--- assets/Unit/UnitSoldier.gd
+++ assets/Unit/UnitSoldier.gd
@@ -1,8 +1,12 @@
 extends Node
 
 
+var is_selected : bool = false
+
 onready var _ObjectTypeNode = get_node("%ObjectType")
 onready var _NavAgentNode = get_node_or_null("%NavigationAgent")
+onready var UnitMesh = $Unit/MeshBody
+onready var shader:ShaderMaterial = UnitMesh.mesh.material.next_pass
 
 
 func _ready():
@@ -14,10 +18,27 @@ func _ready():
 	pass
 
 
-func _on_NavigationAgent_velocity_computed(safe_velocity: Vector3) -> void:
+func _on_NavigationAgent_velocity_computed(_safe_velocity: Vector3) -> void:
 	pass # Replace with function body.
 
 
 func SetAgentTarget(newAgentTarget:Vector3)-> void:
 	print_debug("SetAgentTarget: %s" % newAgentTarget)
 	_NavAgentNode.set_target_location(newAgentTarget)
+
+
+func can_objectselected() -> bool:
+	return _ObjectTypeNode.can_selected()
+
+
+func get_objecttype():
+	return _ObjectTypeNode._ObjectType
+
+
+func select_object(selected:bool) -> void:
+	print_debug("BuildingRoot::select_object() -> Building selected: %s" % selected)
+	is_selected = selected
+	if is_selected:
+		shader.set_shader_param("strenght", 1.0)
+	else:
+		shader.set_shader_param("strenght", 0.0)
