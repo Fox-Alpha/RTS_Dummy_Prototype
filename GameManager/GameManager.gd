6eb1d40340495f5b3aa088d0b30b85275825d266
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-11-09 16:54:09 +0100

    Test: Übergeben einer Alibi Position an Unit NavAgent

--- GameManager/GameManager.gd
+++ GameManager/GameManager.gd
@@ -108,7 +108,7 @@ func _navigate_object() -> void:
 						print_debug(om)
 						var so = om.selected_object
 						if is_instance_valid(so):
-							so.SetAgentTarget(mousePos)
+							so.SetAgentTarget(Vector3(0.0, 0.0, 0.0))
 
 
 
