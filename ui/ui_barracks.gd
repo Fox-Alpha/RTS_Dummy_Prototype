60b10f3fc06564e3d5e625d74a097a6d57e16c87
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-13 00:27:41 +0100

    Add: UI Script u.a. für Button ereignisse Add: ButtonClick ===: Übergibt ereignis an ObjectManager -> BuildingManager -> UnitManager ===: Um neue Einheiten zu erstellen und diesen die ausgewählte Farbe zuzuweisen ===: Dies soll unterschiedliche Einheiten simulieren

---
+++ ui/ui_barracks.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# TODO: Verbinden mit Building Manager
+# Verbinden mit UnitManager
+# Unit Asset abrufen
+# Unit erzeugen im Unitmanager
+
+
+func _on_TextureButton_pressed(arg_1:int):
+	match arg_1:
+		1:	# Unit RED
+			pass
+		2:	# Unit YELLOW
+			pass
+		3:	# Unit GREEN
+			pass
+		4:	# Unit BLUE
+			pass
+	pass # Replace with function body.
+
+
+func _on_TextureButton_color_pressed(extra_arg_0):
+	pass # Replace with function body.
