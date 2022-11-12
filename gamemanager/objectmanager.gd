e0cccd964ab5fad5c1596434f2e0f3c55c3d5209
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-12 17:50:47 +0100

    Chg: Nicht verwendete Variablen markiert Add: Check ob die Methode zum prüfen eines UI im Object vorhanden ist Add: Methodenaufruf an neuen Namen angepasst

--- gamemanager/objectmanager.gd
+++ gamemanager/objectmanager.gd
@@ -34,12 +34,12 @@ func _ready():
 func init_signals() -> void:
 	# Object selektieren
 	if not Signalbus.is_connected("objectselected", self, "_on_select_object"):
-		var sig = Signalbus.connect("objectselected", self, "_on_select_object")
+		var _sig = Signalbus.connect("objectselected", self, "_on_select_object")
 #		assert(sig == OK, "ObjectManager::init_signals() -> connect objectselected failed")
 
 	# Objecte Selektion aufheben
 	if not Signalbus.is_connected("objectunselected", self, "_on_unselect_object"):
-		var sig = Signalbus.connect("objectunselected", self, "_on_unselect_object")
+		var _sig = Signalbus.connect("objectunselected", self, "_on_unselect_object")
 #		assert(sig == OK, "ObjectManager::init_signals() -> connect objunectselected failed")
 #	pass
 
@@ -52,9 +52,10 @@ func _on_unselect_object() -> void:
 	if is_instance_valid(selected_object):
 
 			var uim = GM.get_manager_instance("UIManager")
-			if is_instance_valid(uim):
-				if selected_object.GetBuildingHasUI():
-					uim.show_ui_building(selected_object.GetBuildingUIName(), false)
+			if selected_object.has_method("GetObjectHasUI"):
+				if is_instance_valid(uim):
+					if selected_object.call("GetObjectHasUI"):
+						uim.show_ui_building(selected_object.GetObjectUIName(), false)
 					
 			selected_object.select_object(false)
 			selected_object = null
@@ -83,8 +84,8 @@ func _on_select_object(_selectedobject) -> void:
 			# TODO: UI Anzeigen
 			var uim = GM.get_manager_instance("UIManager")
 			if is_instance_valid(uim):
-				if _selectedobject.GetBuildingHasUI():
-					uim.show_ui_building(_selectedobject.GetBuildingUIName(), true)
+				if _selectedobject.GetObjectHasUI():
+					uim.show_ui_building(_selectedobject.GetObjectUIName(), true)
 		Globals.OBJECT_TYPE_ENUM.TYPE_UNIT:
 			print_debug("Object Type is UNIT")
 		Globals.OBJECT_TYPE_ENUM.TYPE_RESOURCE:
@@ -104,7 +105,7 @@ func set_selectedobject(value) -> void:
 
 
 func get_selectedobject():
-#	return selected_object
+	return selected_object
 	pass
 # ===========================
 
