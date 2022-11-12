0626974d1f4263b1f8c14f5881df725e67b7cbba
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-12 23:35:08 +0100

    Upd: ManagerBase aufbereitet Add: Base Methode zum setzen der GameManager Referenz Chg: Prints Ausgaben angepasst

--- scripts/baseclasses/managerbase.gd
+++ scripts/baseclasses/managerbase.gd
@@ -2,29 +2,37 @@ extends Node
 class_name ManagerBase
 
 # ===========================
-# Bas Klasse von der alle Managerklassen abgeleitet werden
+# Base Klasse von der alle Managerklassen abgeleitet werden
 #
-# Nur gemeinsame Eigenschaften und Funktionen
+# Nur gemeinsame Eigenschaften und Methoden
+# ===========================
+var GM:Node
+
+# ===========================
+# Build-In Methoden
+# ===========================
+#func _init():
+#	print("ManagerBase _init")
+#	print("Instanz::_init() -> ", name)
+##	pass
 #
 #
+func _ready():
+#	print("ManagerBase _ready()")
+	print("Instanz::_ready() -> ", name)
+#	pass
+
+
+#func _enter_tree():
+#	pass
 #
+#
+#func _exit_tree():
+#	pass
 # ===========================
 
-
-
-func _init():
-	print("ManagerBase _init")
-	pass
-
-
-func _ready():
-	print("ManagerBase _ready()")
-	pass
-
-
-func _enter_tree():
-	pass
-
-
-func _exit_tree():
-	pass
+###################
+func SetGameManagerInstance():
+	GM = Globals.get_gamemanager_instance()
+	print(name, "::SetGameManagerInstance() -> ", name)
+###################
