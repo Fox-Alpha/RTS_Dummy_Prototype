f0f43ac64b4710e6331290c08d617bb72f588717
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-13 17:10:07 +0100

    Rem: Signal Connect im falschen Manager

--- gamemanager/buildingmanager.gd
+++ gamemanager/buildingmanager.gd
@@ -15,10 +15,6 @@ export var Buildings : Dictionary = {}
 # Build-in Methoden
 # ===========================
 func _ready():
-	# Ein neues Object instantiieren
-	if not Signalbus.is_connected("instantiatenewobject", self, "_on_instantiate_new_object"):
-		var _sig = Signalbus.connect("instantiatenewobject", self, "_on_instantiate_new_object")
-#		assert(sig == OK, "ObjectManager::init_signals() -> connect objunectselected failed")
 	pass
 
 
