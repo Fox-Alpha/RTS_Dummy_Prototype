346bf47f79b58055d4d0d4fdeab29f84f0180a89
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-13 02:31:26 +0100

    Add: Progressbar für den Build Cooldown Add: Timer für Cooldown Add: Kommentarblöcke erstellt

--- ui/ui_barracks.gd
+++ ui/ui_barracks.gd
@@ -1,11 +1,29 @@
 extends Control
-
-
 # TODO: Verbinden mit Building Manager
 # Verbinden mit UnitManager
 # Unit Asset abrufen
 # Unit erzeugen im Unitmanager
 
+var GM : GameManager
+
+# ===========================
+# Build-In Methoden
+# ===========================
+#func _init():
+#	pass
+
+
+func _ready():
+	_Set_GameManager_Instance()
+
+
+#func _enter_tree():
+#	pass
+
+
+#func _exit_tree():
+#	pass
+# ===========================
 
 func _on_TextureButton_pressed(arg_1:int):
 	match arg_1:
@@ -17,8 +35,25 @@ func _on_TextureButton_pressed(arg_1:int):
 			pass
 		4:	# Unit BLUE
 			pass
+	var t: Timer = get_node("CooldownTimer")
+	if t.is_stopped():
+		get_node("%TextureProgress").value = 0
+		t.start()
 	pass # Replace with function body.
 
 
 func _on_TextureButton_color_pressed(extra_arg_0):
 	pass # Replace with function body.
+
+
+func _Set_GameManager_Instance():
+	GM = Globals.get_gamemanager_instance()
+	print(name, "::SetGameManagerInstance() -> ", name)
+
+
+func _on_Timer_timeout():
+	var pb = .get_node("%TextureProgress")	#ColorRect/PanelContainer/VBoxContainer/Panel/VBoxContainer/
+	pb.value += 1
+	if pb.value == pb.max_value:
+		.get_node("CooldownTimer").stop()
+	pass # Replace with functionC body.
