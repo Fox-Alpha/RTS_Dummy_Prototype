0cc82c33c560b4ea5e02a7bb812a9b0d78e5ab7b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-13 17:05:33 +0100

    Add: Eigenschaften initialisierung

--- scripts/buildings/baracks.gd
+++ scripts/buildings/baracks.gd
@@ -2,12 +2,32 @@ extends BuildingBase
 class_name Barracks
 
 
+#var ObjectTypeProperties:Dictionary = {
+#	"BuildingName": "EmptyName",
+#	"BuildingType": "EmptyType",
+#	"ObjectType": Globals.OBJECT_TYPE_ENUM.TYPE_BUILDING, 
+#	"BuildingHasUI": false,
+#	"BuildingUiName": "EmptyUI",
+#	"BuildingCanSpawnObjects": false,
+#	"ObjectSpawnPos": Vector3(),
+#	"UnitRallyPos": Vector3(),
+#	"ObjectsToSpawn": {}
+#}
+
+
 func _ready():
-	_buildingtype = "BuildingArmyBarrack"
-	_buildingname = "Army Barrack"
-	_buildinghasui = true
-	_buildinguiname = "UI_Barracks"
+	_initobjectproperties()
 #	pass
 
 
-
+func _initobjectproperties():
+	ObjectTypeProperties["BuildingName"] = "Army Barrack"
+	ObjectTypeProperties["BuildingType"] = "BuildingArmyBarrack"
+	ObjectTypeProperties["ObjectType"] = Globals.OBJECT_TYPE_ENUM.TYPE_BUILDING
+	ObjectTypeProperties["BuildingHasUI"] = true
+	ObjectTypeProperties["BuildingUiName"] = "UI_Barracks"
+	ObjectTypeProperties["BuildingCanSpawnObjects"] = true
+	ObjectTypeProperties["ObjectSpawnPos"] = UnitSpawnPos
+	ObjectTypeProperties["UnitRallyPos"] = UnitRallyPos
+	ObjectTypeProperties["ObjectsToSpawn"] = {}
+#	ObjectTypeProperties[""] = ""
