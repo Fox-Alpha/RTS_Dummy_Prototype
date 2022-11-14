80e5ad063e8195ab0add81248890bca0429dc53b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-14 23:06:02 +0100

    Add: GameManager Instanz Chg: Ui aufrufen bei on_select / verbergen bei on_unselect Add: Signal _on_instantiate_new_object

--- assets/Buildings/buildingroot.gd
+++ assets/Buildings/buildingroot.gd
@@ -8,6 +8,9 @@ onready var _ObjectTypeNode = get_node("%ObjectType")
 onready var BuildingMesh = $BaracksBody/MeshBuilding
 onready var shader:ShaderMaterial = BuildingMesh.mesh.material.next_pass
 
+var ui_manager
+var GM : Node
+
 
 # ===========================
 # Build-In Methoden
@@ -18,6 +21,12 @@ onready var shader:ShaderMaterial = BuildingMesh.mesh.material.next_pass
 
 func _ready():
 	shader = BuildingMesh.mesh.material.next_pass
+	
+	GM = Globals.get_gamemanager_instance()
+	if not Signalbus.is_connected("newobject_instantiated", self, "_on_instantiate_new_object"):
+		var _sig = Signalbus.connect("newobject_instantiated", self, "_on_instantiate_new_object")
+	# if is_instance_valid(GM):
+		# ui_manager = GM.get_manager_instance("UIManager")
 
 
 #func _enter_tree():
@@ -32,6 +41,33 @@ func _ready():
 #func _process(delta):
 #	pass
 # ===========================
+func _on_instantiate_new_object(newType:int):
+	print_debug("OM: Neuen Typ erstellen: ", newType)
+	
+	var objtype = get_objecttype()
+	match objtype:
+		Globals.OBJECT_TYPE_ENUM.TYPE_BUILDING:
+			print_debug("BuildNewObject(): Object Type is Building")
+			# var bm = GM.get_manager_instance("BuildingManager")
+			# if is_instance_valid(bm):
+				# bm.
+		Globals.OBJECT_TYPE_ENUM.TYPE_UNIT:
+			print_debug("BuildNewObject(): Object Type is UNIT")
+		Globals.OBJECT_TYPE_ENUM.TYPE_RESOURCE:
+			print_debug("BuildNewObject(): Object Type is RESOURCE")
+		Globals.OBJECT_TYPE_ENUM.TYPE_GROUND:
+			print_debug("BuildNewObject(): Object Type is GROUND")
+		Globals.OBJECT_TYPE_ENUM.TYPE_UNDEFINED:
+			print_debug("BuildNewObject(): Object Type is UNDEFINED")
+	# pass
+
+
+func _manage_ui(showui:bool):
+	ui_manager = GM.get_manager_instance("UIManager")
+
+	if is_instance_valid(ui_manager):
+		if GetObjectHasUI():
+			ui_manager.show_ui_instance(GetObjectUIName(), showui, GetObjectProperties())
 
 
 func select_object(selected:bool) -> void:
@@ -40,9 +76,11 @@ func select_object(selected:bool) -> void:
 	if _ObjectTypeNode.building_is_selected:
 		shader.set_shader_param("strenght", 1.0)
 		# TRYME: UI Anzeigen lassen
+		_manage_ui(true)
 	else:
 		shader.set_shader_param("strenght", 0.0)
 		# TRYME: UI Ausblenden lassen
+		_manage_ui(false)
 
 
 func GetObjectProperties() -> Dictionary:
