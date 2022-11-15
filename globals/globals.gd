b3e5f9d245ea955d5903885690c6b494fe3a7b91
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-11-15 23:05:32 +0100

    Chg: Signal game_manager_is_ready deaktiviert

--- globals/globals.gd
+++ globals/globals.gd
@@ -17,16 +17,16 @@ func _ready():
 	if !Signalbus.is_connected("setgamemanagerinstance", self, "set_gamemanager_instance"):
 		var _sig = Signalbus.connect("setgamemanagerinstance", self, "set_gamemanager_instance")
 
-	if !Signalbus.is_connected("game_manager_is_ready", self, "on_game_manager_is_ready"):
-		var _sig = Signalbus.connect("game_manager_is_ready", self, "on_game_manager_is_ready")
+#	if !Signalbus.is_connected("game_manager_is_ready", self, "on_game_manager_is_ready"):
+#		var _sig = Signalbus.connect("game_manager_is_ready", self, "on_game_manager_is_ready")
 #		assert(sig == OK, "GLOBALS::_ready() - > Connect on_game_manager_is_ready fehlgeschlagen")
 #	pass
 
-func on_game_manager_is_ready():
-	var gm = get_gamemanager_instance()
-	if is_instance_valid(gm):
-		gm.BroadCastGM()
-	pass
+#func on_game_manager_is_ready():
+#	var gm = get_gamemanager_instance()
+#	if is_instance_valid(gm):
+#		gm.BroadCastGM()
+#	pass
 	
 func set_gamemanager_instance(value):
 	if value != null && _instance == null && value != _instance:
