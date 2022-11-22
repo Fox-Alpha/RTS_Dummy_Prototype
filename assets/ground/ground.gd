45b0116b02eef6ce0fc3e6ba2d412bb61016218a
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-11-22 12:31:19 +0100

    Add: Registrieren für GameManager Ready Signal Add: Bereitstellen der Groundnode ID im Gamemanager nach GM_Ready

--- assets/ground/ground.gd
+++ assets/ground/ground.gd
@@ -7,6 +7,8 @@ onready var _ObjectTypeNode = get_node("%ObjectType")
 
 func _ready():
 	_ObjectTypeNode._ObjectType = Globals.OBJECT_TYPE_ENUM.TYPE_GROUND
+	if !Signalbus.is_connected("game_manager_is_ready", self, "_on_game_manager_is_ready"):
+		var _sig = Signalbus.connect("game_manager_is_ready", self, "_on_game_manager_is_ready")
 	pass
 
 
@@ -20,3 +22,10 @@ func get_objecttype_node() -> Node:
 
 func get_objecttype():
 	return _ObjectTypeNode._ObjectType
+
+
+func _on_game_manager_is_ready():
+	# TODO: Node instance ID im GameManager (?? WorldManager) anmelden
+	# TODO: Eigener Verweis auf GameManager bei bedarf erstellen
+	Globals._instance.GroundNodeID = get_instance_id()
+	pass
