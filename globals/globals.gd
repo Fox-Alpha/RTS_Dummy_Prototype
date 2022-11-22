d75a23662d9719b597e4db3e21a4bb1395d3c5e4
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-11-22 15:34:31 +0100

    Ren: Globale GameManager Instanz umbenannt

--- globals/globals.gd
+++ globals/globals.gd
@@ -17,27 +17,27 @@ enum OBJECT_TYPE_ENUM{
 
 # Globale GameManager Instanz
 # CHGME: getter und setter als private
-var _instance:GameManager setget set_gamemanager_instance, get_gamemanager_instance
+var GMInstance:GameManager setget _set_gamemanager_instance, _get_gamemanager_instance
 
 onready var _WorldNavPointNodeParent = get_tree().get_root().get_node("./Main/World/NavPoints")
 
 func _ready():
-	if !Signalbus.is_connected("setgamemanagerinstance", self, "set_gamemanager_instance"):
-		var _sig = Signalbus.connect("setgamemanagerinstance", self, "set_gamemanager_instance")
+	if !Signalbus.is_connected("setgamemanagerinstance", self, "_set_gamemanager_instance"):
+		var _sig = Signalbus.connect("setgamemanagerinstance", self, "_set_gamemanager_instance")
 
 	if !Signalbus.is_connected("scene_end_reached", self, "_on_scene_end_reached"):
 		var _sig = Signalbus.connect("scene_end_reached", self, "_on_scene_end_reached")
 
 
 func _on_scene_end_reached():
-	if is_instance_valid(_instance):
+	if is_instance_valid(GMInstance):
 		Signalbus.emit_signal("game_manager_is_ready")
 
 
-func set_gamemanager_instance(value):
-	if value != null && _instance == null && value != _instance:
-		_instance = value
+func _set_gamemanager_instance(value):
+	if value != null && GMInstance == null && value != GMInstance:
+		GMInstance = value
 
 
-func get_gamemanager_instance():
-	return _instance
+func _get_gamemanager_instance():
+	return GMInstance
