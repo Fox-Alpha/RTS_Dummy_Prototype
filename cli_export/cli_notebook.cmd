af1e2ed6e888164d77f3abc536e5eefecc7a3954
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-12-08 16:55:28 +0100

    Upd: Dateinamen und Pfade für Export auf Notebook angepasst.

--- cli_export/cli_notebook.cmd
+++ cli_export/cli_notebook.cmd
@@ -1,12 +1,13 @@
 @echo off
+cls
 :: P:\SteamLibrary\steamapps\common\Godot Engine>
 REM godot.windows.opt.tools.64.exe --verbose --export-debug "Windows Desktop" --test gui --path "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame" "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame\.Export\MiniShooter.exe" > "P:\3. GODOT Projekte\Projekte\Just4Fun_Mini_Shootergame\.export\export.txt"
 
-cd C:\Proggen\Godot\Godot_v3.5.1-stable_win64
-Godot_v3.5.1-stable_win64.exe --verbose --export-debug "Windows Desktop" --test gui --path "C:\Proggen\Godot\Projekte\RTS_Dummy_Prototype" "C:\Proggen\Godot\Projekte\Export\RTS_Dummy_Prototype\RTS_Dummy_Prototype.exe" > "C:\Proggen\Godot\Projekte\Export\RTS_Dummy_Prototype\export.log.txt"
+cd "C:\Proggen\DevProgramme\Godot Engine"
+Godot-stable.exe --verbose --export-debug "Windows Desktop" --path "C:\Proggen\01 Godot Projekte\RTS_Dummy_Prototype" "C:\Proggen\01 Godot Projekte\RTS_Dummy_Prototype\.export\RTS_Dummy_Prototype.exe" > "C:\Proggen\01 Godot Projekte\RTS_Dummy_Prototype\.export\export.log.txt"
 
-cd C:\Proggen\Godot\Projekte\Export\RTS_Dummy_Prototype
+cd "C:\Proggen\01 Godot Projekte\RTS_Dummy_Prototype\.export"
 RTS_Dummy_Prototype.exe
 
 
-cd C:\Proggen\Godot\Projekte\RTS_Dummy_Prototype
+cd "C:\Proggen\01 Godot Projekte\RTS_Dummy_Prototype\cli_export"
