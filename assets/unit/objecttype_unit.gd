7a50736ab18f35d2d42d46f494a2b55c0a0681f9
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-12-08 22:12:28 +0100

    Add: Properties

--- assets/unit/objecttype_unit.gd
+++ assets/unit/objecttype_unit.gd
@@ -9,8 +9,8 @@ extends UnitBase
 #	pass
 
 
-#func _ready():
-#	pass
+func _ready():
+	_initobjectproperties()
 
 
 #func _enter_tree():
@@ -20,3 +20,18 @@ extends UnitBase
 #func _exit_tree():
 #	pass
 # ===========================
+
+func _initobjectproperties():
+	ObjectTypeProperties["ObjectParentName"] = _UnitRootNode.name
+	ObjectTypeProperties["ObjectInstanceID"] = get_parent().get_instance_id()
+	ObjectTypeProperties["ObjectType"] = Globals.OBJECT_TYPE_ENUM.TYPE_UNIT
+	ObjectTypeProperties["ObjectName"] = "Army Soldier"
+	ObjectTypeProperties["ObjectTypeName"] = "UnitArmySoldier"
+	ObjectTypeProperties["ObjectCanSelect"] = true
+	ObjectTypeProperties["ObjectHasUI"] = false
+	ObjectTypeProperties["ObjectUI_ID"] = -1
+	ObjectTypeProperties["ObjectUiName"] = "UI_Soldier"
+	ObjectTypeProperties["ObjectCanSpawnObjects"] = false
+	ObjectTypeProperties["ObjectSpawnPos"] = ""
+	ObjectTypeProperties["ObjectRallyPos"] = ""
+	ObjectTypeProperties["ObjectsToSpawn"] = {}
