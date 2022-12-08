aa0b597721ec763fa17c824caf9ff6eb1f124f80
Fox-Alpha <fox-alpha@tarnhoerner.de>
2022-12-08 22:13:52 +0100

    Add: Zugriff auf Properties per public variablen

--- scripts/baseclasses/unitbase.gd
+++ scripts/baseclasses/unitbase.gd
@@ -1,18 +1,39 @@
 class_name UnitBase
 extends Node
 
+var unit_is_selected:bool setget _set_unit_is_selected, _get_unit_is_selected 
 
-export var UnitName : String = "EmptyUnitName" setget set_unit_name, get_unit_name
-export var UnitType : String = "EmptyUnitType" setget set_unit_type, get_unit_type
+var UnitObjectType:int setget _set_objecttype, _get_objecttype # OBJECT_TYPE_ENUM.TYPE_*
+var UnitName : String setget set_unit_name, get_unit_name
+var UnitTypeName : String setget _set_unit_typename, _get_unit_typename
+var UnitCanSelect : bool setget _set_unit_can_select, _get_unit_can_select
+var UnitHasUI : bool setget _set_unithasui, _get_unithasui
+var UnitUiName : String setget _set_unit_uiname, _get_unit_uiname
+var UnitCanSpawnUnits : bool setget _set_canspawnunits, _get_canspawnunits
+var UnitSpawnPos : Vector3 setget _set_unit_spawnpoint, _get_unit_spawnpoint
+var UnitRallyPos : Vector3 setget _set_unit_rallypoint, _get_unit_rallypoint
+var UnitUI_ID : int setget _set_unit_ui_id, _get_unit_ui_id
+# TODO: liste mit Variablen für den Public Zugriff vervollständigen
+# ObjectParentName
+# ObjectInstanceID
+# ObjectType
+# ObjectUI_ID
+# ObjectsToSpawn
+# "ObjectsToSpawn": {}
+
+#export var UnitName : String = "EmptyUnitName" setget set_unit_name, get_unit_name
+#export var UnitType : String = "EmptyUnitType" setget set_unit_type, get_unit_type
 
 export var _canselectunit : bool = true
 export var _canmoveunit : bool = true
 
-var _ObjectType = Globals.OBJECT_TYPE_ENUM.TYPE_UNDEFINED
+var _ObjectType = Globals.OBJECT_TYPE_ENUM.TYPE_UNIT
 
 #onready var GM = Globals.get_gamemanager_instance()
 
-onready var UnitRootNode = owner
+# onready var UnitRootNode = owner
+
+onready var _UnitRootNode = get_parent()
 
 
 #-------------------------------------------------------------------------------
