77a8333226bc9c01c89dc666c55dcd51f2c913da
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-12-09 14:47:55 +0100

    Add: Obsolete Notiz ===: Zum entfernen markiert

--- scripts/unitclass/simpleunit.gd
+++ scripts/unitclass/simpleunit.gd
@@ -1,9 +1,10 @@
 class_name SimpleUnit
 extends UnitBase
 
+# REMOVE: Klasse ist obsolete. Mögliche Referenzen entfernen
 
 func _ready():
 	_ObjectType = Globals.OBJECT_TYPE_ENUM.TYPE_UNIT
 	UnitName = "Simple Dummy Unit"
-	UnitType = "SimpleDummyUnit"
+	# UnitType = "SimpleDummyUnit"
 	pass
