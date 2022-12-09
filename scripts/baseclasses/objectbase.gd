8f12d64f04a967c6c6d21c0f42077b894b153de9
Dirk Buck <dirk.buck@wolsdorff-tobacco.de>
2022-12-09 14:46:05 +0100

    Add: Neue Basisklasse für interaktive Objecte ===: Zusammenführen der Basisklasse

---
+++ scripts/baseclasses/objectbase.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name ObjectBase
+
+# TODO: ObjectBase erstellen
+## Generalisieren von objecttypes
+## Erstellen einer Basisklasse von der alle Interaktiven Objekte erben können
+## Zusammenführen von BuildingBase und UnitBase
